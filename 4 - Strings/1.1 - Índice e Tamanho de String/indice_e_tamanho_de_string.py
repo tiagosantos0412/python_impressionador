77cf8c6a020ce7ab3d6fdde7c6b855cedfd320bc
Tiago Felipe <tiagosantos0412@gmail.com>
2023-12-20 13:41:10 -0300

    Índice e Tamanho de String

---
+++ 4 - Strings/1.1 - Índice e Tamanho de String/indice_e_tamanho_de_string.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Indice e Tamanho de String.ipynb
+
+
+0 1 2 3 4 5 6 7 8 9 10 11 12 13
+l i r a @ g m a i l  .  c  o m
+
+Pegar um item de uma string: texto[índice]
+Pegar o tamanho de um texto: len(texto)
+"""
+
+email = 'lira@gmail.com'
+nome = 'João Lira'
